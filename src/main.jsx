72a92f9c7527534a026d64aaba02566c2e42ddca
paully0408 <paulashton001@gmail.com>
2026-04-02 13:44:52 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -710,3 +710,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
